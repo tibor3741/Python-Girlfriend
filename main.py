4c980e2391ce71e4062cf6b0abca0a01d12bc224
Tibor <36704519+tibor3741@users.noreply.github.com>
2020-04-23 22:05:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,109 +6,47 @@ import random
 clear = lambda: os.system('cls' if os.name=='nt'else 'clear')
 clear()
 greatings = ['Üdvözöllek','Szia','Helló','Halli','Hellóka'];
-rnd = random.choice(greatings);
+rnd1 = random.choice(greatings);
 time.sleep(3)
-print(rnd+' az én nevem Kasszandra')
+print(rnd1+' az én nevem Kasszandra')
 time.sleep(2)
 nev = input("Téged hogy hívnak?: ")
 while len(nev)== 0:
   clear()
-  print(rnd+' az én nevem Kasszandra')  
+  print(rnd1+' az én nevem Kasszandra')  
   nev = input("Téged hogy hívnak?: ")
   
 eletkor = int(input("Hány éves vagy?: "))
 greatings2 = ['Szia','Helló','Halli'];
-rnd = random.choice(greatings2);
-print(rnd+ " {}.".format(nev))
+rnd2 = random.choice(greatings2);
+print(rnd2+ " {}.".format(nev))
 time.sleep(2)
 print('Milyen színű haj tetsszik neked?\n 1.Vörös \n 2.Barna \n 3.Fekete \n 4.Szöke \n 5.Őssz')
-haj_szin = int( input ('Válasz egy számot: '))
+haj_szin = int(input("válasz egy számot: "))
 time.sleep(1)
-print('Milyen haj viselet teszik: \n 1.Rövid Göndör \n 2.Rövid Egyenes \n 3.Rövid Hulámos \n 4.Hosszú Egyenes \n 5.Hosszú Hulámos \n 6.Hosszú Göndör ')
+print('Milyen haj viselet teszik: \n 1.Rövid Göndör \n 2.Rövid Egyenes \n 3.Rövid Hullámos\n 4.Hosszú Egyenes \n 5.Hosszú Hulámos \n 6.Hosszú Göndör ')
 haj_viselet = int(input ('Válasz egy számot: '))
-#Hajviselet 1
-#-----------------------------------------------------
-if haj_szin == 1 and haj_viselet == 1:
-  print('Nekem Pont Vörös és Rövid Göndör a Hajam')
-elif haj_szin == 2 and haj_viselet == 1:
-  print('Nekem Pont Barna és Rövid Göndör a Hajam')
-elif haj_szin == 3 and haj_viselet == 1:
-  print('Nekem Pont Fekete és Rövid Göndör a Hajam')
-elif haj_szin == 4 and haj_viselet == 1:
-  print('Nekem Pont Szőke és Rövid Göndör a Hajam')
-elif haj_szin == 5 and haj_viselet == 1:
-  print('Nekem Pont Őssz és Rövid Göndör a Hajam')
-#Hajviselet 2
-# ------------------------------------------------------
-elif haj_szin == 1 and haj_viselet == 2:
-  print('Nekem Pont Vörös és Rövid Egyenes a Hajam')
-elif haj_szin == 2 and haj_viselet == 2:
-  print('Nekem Pont Barna és Rövid Egyenes a Hajam')
-elif haj_szin == 3 and haj_viselet == 2:
-  print('Nekem Pont Fekete és Rövid Egyenes a Hajam')
-elif haj_szin == 4 and haj_viselet == 2:
-  print('Nekem Pont Szöke és Rövid Egyenes a Hajam')
-elif haj_szin == 5 and haj_viselet == 2:
-  print('Nekem Pont Őssz és Rövid Egyenes a Hajam')
-#Hajviselet 3 
-# ------------------------------------------------------
-elif haj_szin == 1 and haj_viselet == 3:
-  print('Nekem Pont Vörös és Rövid Hulámos a Hajam')
-elif haj_szin == 2 and haj_viselet == 3:
-  print('Nekem Pont Barna és Rövid Hulámos a Hajam')
-elif haj_szin == 3 and haj_viselet == 3:
-  print('Nekem Pont Fekete és Rövid Hulámos a Hajam')
-elif haj_szin == 4 and haj_viselet == 3:
-  print('Nekem Pont Szöke és Rövid Hulámos a Hajam')
-elif haj_szin == 5 and haj_viselet == 3:
-  print('Nekem Pont Őssz és Rövid Hulámos a Hajam')
-
-#Hajviselet4
-#----------------------------------------------------------
-elif haj_szin == 1 and haj_viselet == 4:
-  print('Nekem Pont Vörös és Hosszú Egyenes  a Hajam')
-elif haj_szin == 2 and haj_viselet == 4:
-  print('Nekem Pont Barna és Hosszú Egyenes a Hajam')
-elif haj_szin == 3 and haj_viselet == 4:
-  print('Nekem Pont Fekete és Hosszú Egyenes a Hajam')
-elif haj_szin == 4 and haj_viselet == 4:
-  print('Nekem Pont Szőke és Hosszú  Egyenes a Hajam')
-elif haj_szin == 5 and haj_viselet == 4:
-  print('Nekem Pont Őssz és Hosszú Egyenes a Hajam')
-
-#Hajviselet5
-#----------------------------------------------------------
-elif haj_szin == 1 and haj_viselet == 5:
-  print('Nekem Pont Vörös és Hosszú Hulámos  a Hajam')
-elif haj_szin == 2 and haj_viselet == 4:
-  print('Nekem Pont Barna és Hosszú Hulámos a Hajam')
-elif haj_szin == 3 and haj_viselet == 5:
-  print('Nekem Pont Fekete és Hosszú Hulámos a Hajam')
-elif haj_szin == 4 and haj_viselet == 5:
-  print('Nekem Pont Szőke és Hosszú  Hulámos a Hajam')
-elif haj_szin == 5 and haj_viselet == 5:
-  print('Nekem Pont Őssz és Hosszú Hulámos a Hajam')
-
-
-#Hajviselet6
-#----------------------------------------------------------
-elif haj_szin == 1 and haj_viselet == 6:
-  print('Nekem Pont Vörös és Hosszú Göndör a Hajam')
-elif haj_szin == 2 and haj_viselet == 6:
-  print('Nekem Pont Barna és Hosszú Göndör a Hajam')
-elif haj_szin == 3 and haj_viselet == 6:
-  print('Nekem Pont Fekete és Hosszú Göndör a Hajam')
-elif haj_szin == 4 and haj_viselet == 6:
-  print('Nekem Pont Szőke és Hosszú Göndör a Hajam')
-elif haj_szin == 5 and haj_viselet == 6:
-  print('Nekem Pont Őssz és Hosszú Göndör a Hajam')
-  
-  
+haj_szinek = ["","Vörös", "Barna", "Fekete","Szőke", "Ősz"]
+haj_viseletek = ["","Rövid Göndör","Rövid Egyenes","Rövid Hulámos","Hosszú Egyenes","Hosszú Hulámos","Hosszú Göndör"]
+print("Nekem Pont "+ haj_szinek[haj_szin]+" és "+  haj_viseletek[haj_viselet]+" a Hajam")
 time.sleep(3)
 print('Szeretsz engem? \n Igen \n Nem ')
-
-
 szeret = input('Mit választasz: ')
+while len(szeret)== 0:
+  clear()
+  print(rnd1 +' az én nevem Kasszandra')
+  print("Téged hogy hívnak {}".format(nev))
+  print('Milyen színű haj tetsszik neked?\n 1.Vörös \n 2.Barna \n 3.Fekete \n 4.Szöke \n 5.Őssz')
+  print('Válasz egy számot: {}'.format(haj_viselet))
+  print('Milyen haj viselet teszik: \n 1.Rövid Göndör \n 2.Rövid Egyenes \n 3.Rövid Hullámos\n 4.Hosszú Egyenes \n 5.Hosszú Hulámos \n 6.Hosszú Göndör ')
+  print('Válasz egy számot: {}'.format(haj_szin))
+  print("Nekem Pont "+ haj_szinek[haj_szin]+" és "+  haj_viseletek[haj_viselet]+" a Hajam")
+
+  print('Szeretsz engem? \n Igen \n Nem ')
+  szeret = input('Mit választasz: ')
+
+
+  
 
 if  szeret.casefold() == "igen" :
    print('Én is Szeretlek')
@@ -128,5 +66,3 @@ time.sleep(3)
 print(rnd+ " {}.".format(nev))
 time.sleep(2)
 exit()
-
-
