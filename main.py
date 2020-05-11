7fe9a23b6ff412b5be89350ffdac31e5c0b34bcc
Tibor <36704519+tibor3741@users.noreply.github.com>
2020-05-11 20:10:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,53 @@
 #!/usr/bin/python3
 #-*- coding:utf-8 -*-
+
 import os
 import time
 import random
+
 # Függvények
 # ==========
 # Képernyő törlése
-clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
-   
-              
+clear = lambda: os.system('cls' if os.name=='nt' else 'clear')             
+
+#Egy Szöveges kérdés a felhasználótól
 def user_kerdez(kerdes):
-	valasz = input(kerdes)
-	while len(valasz)== 0:
-		print('Mit akartál kérdezni ?')
-		valasz = input(kerdes)
-	return valasz  
-    
+        valasz = input(kerdes)
+        while len(valasz)== 0:
+                print('Mit akartál kérdezni ?')
+                valasz = input(kerdes)
+        return valasz  
+#Egy szöveges Kérsdés    
 def kerdez(kerdes):
-	valasz = input(kerdes)
-	while len(valasz)== 0:
-		print('Mondd el légyszi! ')
-		valasz = input(kerdes)
-	return valasz
-        # Eldönti, hogy igen, vagy nem
-def eldont(kerdes):
-            while True:
-		valasz = input(kerdes).casefold()
-		if valasz in ['i', 'igen', 'persze', 'nagyon']:
-			return True
-		elif valasz in ['n', 'nem', 'dehogy']:
-			return False
-		else:
-			print('Ezt nem értem. ')
- # Választás egy listából a sorszám használatávan
-def valaszt(kerdes, opciok):
-        print(kerdes)
-        for i in range(len(opciok)):
-                print(str(i+1) + '. ' + opciok[i])
-        valasz = int(input('Válassz egy számot: ')) - 1
-        while valasz < 0 or valasz >= len(opciok):
-                print('Ilyen sorszámú opció nincs! ')
-                valasz = int(input('Válassz egy számot: ')) - 1
+        valasz = input(kerdes)
+        while len(valasz)== 0:
+                print('Mondd el légyszi! ')
+                valasz = input(kerdes)
         return valasz
+# Választás egy listából a sorszám használatávan
+def valaszt(kerdes, opciok):
+    print(kerdes)
+    for i in range(len(opciok)):
+        print(str(i+1) + '. ' + opciok[i])
+    valasz = int(input('Válassz egy számot: ')) - 1
+    while valasz < 0 or valasz >= len(opciok):
+        print('Ilyen sorszámú opció nincs! ')
+        valasz = int(input('Válassz egy számot: ')) - 1
+    return valasz
+    
+# Eldönti, hogy igen, vagy nem
+def eldont(kerdes):
+        while True:
+            valasz = input(kerdes).casefold()
+            if valasz in ['i', 'igen', 'persze', 'nagyon']:
+                return True
+            elif valasz in ['n', 'nem', 'dehogy']:
+                return False
+            else:
+                print('Ezt nem értem. ')
+
+
+
 #Változók
 #--------------------------------------
 # Beállítások
@@ -70,54 +76,53 @@ eletkor = int(kerdez('Hány éves vagy? '))
 
 #ilyen felhasználó van vagy nincs 
 for i in range(len(nev_adat)):
-    if (nev == nev_adat[i]):
-        print('Űdvözöllek újra')
-        user=user_kerdez('')
-        if(user=="szia"or"helló"or"Szia""Helló");
-            print(random.close(greetings),nev);
-        user=user_kerdez('')
-        if(user=="hogy vagy?"or"hogy érzed magad?"):
-            print(random.close(how_are_you));
-        user=user_kerdez('')
-        if(user=="milyen volt a napod?"or"milyen volt a heted ?"or"milyen napod volt?"):
-            print(random.close(how_was_your_day))
+        if (nev == nev_adat[i]):
+           #van ilyen
+            print('Űdvözöllek újra')
+            user = user_kerdez('')
+            if(user=="szia"or"helló"or"Szia""Helló"):
+                print(random.close(greetings), nev)
+            user = user_kerdez('')
+            if(user=="hogy vagy?"or"hogy érzed magad?"):
+                print(random.close(how_are_you))
+            user = user_kerdez('')
+            if(user=="milyen volt a napod?"or"milyen volt a heted ?"or"milyen napod volt?"):
+                print(random.close(how_was_your_day))
+        #már vége a kérdezésnek
         print('mennem kell')
-        print('Szia')
+        print('szaladok')
         exit()
- 
-        
-    else:
+else:
+        #nincs ilyen
         # listához hozzáad valamit
         def rekord(nev, eletkor):
-                
                 nev_adat.append(nev)
                 ev_adat.append (eletkor)
-            clear()
-            time.sleep(2)
+        clear()
+        time.sleep(2)
 
         # Először nem szeret
         szeret = False
         # Addig kérdezgetjük, amíg nem mondja, hogy szeret
         time.sleep(2)
         while not szeret:
-            # Nőideál
-            haj_szin = valaszt('Milyen színű haj tetszik neked?', haj_szinek)
-            haj_hossz = valaszt('Milyen hosszú haj tetszik neked?', haj_hosszok)
-            haj_viselet = valaszt('Milyen haj viselet teszik?', haj_viseletek)
+                # Nőideál
+                haj_szin = valaszt('Milyen színű haj tetszik neked?', haj_szinek)
+                haj_hossz = valaszt('Milyen hosszú haj tetszik neked?', haj_hosszok)
+                haj_viselet = valaszt('Milyen haj viselet teszik?', haj_viseletek)
 
-            # Nahát, pont ilyen a hajam :D
-            print('Nekem Pont', haj_szinek[haj_szin], 'és', haj_hosszok[haj_hossz], haj_viseletek[haj_viselet], 'a Hajam')
-            # Megkérdezzük, hogy szeret-e
-            time.sleep(3)
-            szeret = eldont('Szeretsz engem? ')
-            if not szeret:
+        # Nahát, pont ilyen a hajam :D
+        print('Nekem Pont', haj_szinek[haj_szin], 'és', haj_hosszok[haj_hossz], haj_viseletek[haj_viselet], 'a Hajam')
+        # Megkérdezzük, hogy szeret-e
+        time.sleep(3)
+        szeret = eldont('Szeretsz engem? ')
+        if not szeret:
                 print('Oké, Erre majd visszatérünk')
                 time.sleep(4)
                                
         # Itt már kilépett a ciklusból, szóval biztos, hogy szeret
         print('Én is Szeretlek')
         rekord(nev, eletkor)
-
         # Kedvenc szín
         szin = kerdez('Melyik a kedvenc színed? ')
         print('Nekem is a', szin, 'a kedvencem')
@@ -125,10 +130,6 @@ for i in range(len(nev_adat)):
         # Elköszönés
         print('Most mennem kell')
         print(random.choice(goodbyes), nev)
+exit()
 
-        exit()
-
-
-
-                
 
